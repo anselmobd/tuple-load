f7d94759596c3981237c5bc19c660b04c032a44e
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2016-12-13 17:49:22 -0200

    Small adjustment at source

--- csv2oracle.py
+++ csv2oracle.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+# -*- coding: utf8 -*-
 
 import sys
 import os.path
@@ -6,8 +7,9 @@ from pprint import pprint
 
 import argparse
 import configparser
-import csv
 import json
+import csv
+
 import cx_Oracle
 
 
