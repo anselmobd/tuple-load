f1345395b07345d720d140e19ad800b40f339edd
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2016-12-14 21:31:43 -0200

    Sensible data goto secret.py

---
+++ secret.sample.py
@@ -0,0 +1,19 @@
+# -*- coding: utf8 -*-
+
+# That script is an example of what can be the "secret.py" file
+
+DBSECRET = {
+
+    'db.from.name1': {
+        'username': 'user1',
+        'password': 'pass1',
+        'database': 'DB1',
+        'schema': 'SCHEM1'},
+
+    'db.to.name2': {
+        'username': 'username',
+        'password': 'password',
+        'servicename': 'service',
+        'schema': 'schem'}
+
+    }
