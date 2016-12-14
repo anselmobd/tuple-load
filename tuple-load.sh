cf8a5222a8c2a24fa58ba6933abd973ed8adf1de
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2016-12-14 14:28:35 -0200

    First vertion of configuration to BASI_030

--- tuple-load.sh
+++ tuple-load.sh
@@ -6,6 +6,6 @@
 
 ./csv2oracle.py csv/conta_de_estoque.v161202.csv -d -vvv
 
-./dbms2csv.py ini/insumo_nao_tecido_capa.ini > csv/insumos_nao_tecido_capa.csv
+./dbms2csv.py ini/insumo_nao_tecido_capa.ini > csv/_insumos_nao_tecido_capa.csv
 
 ./csv2oracle.py csv/insumos_nao_tecido_capa.csv -d -vvv
