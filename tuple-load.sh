fb1df2326e582794f82e5fa057321759bfb99f42
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2017-01-13 16:45:02 -0200

    dbms2csv new parameter: CSV file to be created. (no more to output). Starting insumo_tecido. VERBOSE parameter.

--- tuple-load.sh
+++ tuple-load.sh
@@ -1,43 +1,52 @@
 #!/bin/bash
 
+if [ $1 ] ; then
+  VERBOSE=$1
+else
+  VERBOSE="-vvv"
+fi
+
 # generate
 
-./dbms2csv.py insumo_nao_tecido_codigos.ini > csv/_insumo_nao_tecido_codigos.csv
+./dbms2csv.py insumo_nao_tecido_codigos.ini _insumo_nao_tecido_codigos.csv $VERBOSE
 
-./dbms2csv.py insumo_nao_tecido_capa.ini > csv/_insumo_nao_tecido_capa.csv
+./dbms2csv.py insumo_nao_tecido_capa.ini _insumo_nao_tecido_capa.csv $VERBOSE
 
-./dbms2csv.py insumo_nao_tecido_tamanho.ini > csv/_insumo_nao_tecido_tamanho.csv
+./dbms2csv.py insumo_nao_tecido_tamanho.ini _insumo_nao_tecido_tamanho.csv $VERBOSE
 
-./dbms2csv.py insumo_nao_tecido_tamanho_cor.ini > csv/_insumo_nao_tecido_tamanho_cor.csv
+./dbms2csv.py insumo_nao_tecido_tamanho_cor.ini _insumo_nao_tecido_tamanho_cor.csv $VERBOSE
+
+./dbms2csv.py insumo_tecido_codigos.ini _insumo_tecido_codigos.csv $VERBOSE
+
 
 # delete
 
-./csv2oracle.py _insumo_nao_tecido_tamanho_cor.csv -d -vvv
+./csv2oracle.py _insumo_nao_tecido_tamanho_cor.csv -d $VERBOSE
 
-./csv2oracle.py _insumo_nao_tecido_tamanho.csv -d -vvv
+./csv2oracle.py _insumo_nao_tecido_tamanho.csv -d $VERBOSE
 
-./csv2oracle.py _insumo_nao_tecido_capa.csv -d -vvv
+./csv2oracle.py _insumo_nao_tecido_capa.csv -d $VERBOSE
 
-./csv2oracle.py conta_de_estoque.v161202.csv -d -vvv
+./csv2oracle.py conta_de_estoque.v161202.csv -d $VERBOSE
 
-./csv2oracle.py cor.fixo.csv -d -vvv
+./csv2oracle.py cor.fixo.csv -d $VERBOSE
 
-./csv2oracle.py tamanho.fixo.csv -d -vvv
+./csv2oracle.py tamanho.fixo.csv -d $VERBOSE
 
-./csv2oracle.py unidades_de_medida.fixo.csv -d -vvv
+./csv2oracle.py unidades_de_medida.fixo.csv -d $VERBOSE
 
 # insert/update
 
-./csv2oracle.py unidades_de_medida.fixo.csv -i -vvv
+./csv2oracle.py unidades_de_medida.fixo.csv -i $VERBOSE
 
-./csv2oracle.py tamanho.fixo.csv -i -vvv
+./csv2oracle.py tamanho.fixo.csv -i $VERBOSE
 
-./csv2oracle.py cor.fixo.csv -i -vvv
+./csv2oracle.py cor.fixo.csv -i $VERBOSE
 
-./csv2oracle.py conta_de_estoque.v161202.csv -i -vvv
+./csv2oracle.py conta_de_estoque.v161202.csv -i $VERBOSE
 
-./csv2oracle.py _insumo_nao_tecido_capa.csv -i -vvv
+./csv2oracle.py _insumo_nao_tecido_capa.csv -i $VERBOSE
 
-./csv2oracle.py _insumo_nao_tecido_tamanho.csv -i -vvv
+./csv2oracle.py _insumo_nao_tecido_tamanho.csv -i $VERBOSE
 
-./csv2oracle.py _insumo_nao_tecido_tamanho_cor.csv -i -vvv
+./csv2oracle.py _insumo_nao_tecido_tamanho_cor.csv -i $VERBOSE
