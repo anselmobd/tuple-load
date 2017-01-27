156f0a4fc7ced7da3b05d6e5092d607989b7d6fa
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2017-01-27 13:06:00 -0200

    Adding produto_md_capa, renaming and bug

--- tuple-load.sh
+++ tuple-load.sh
@@ -30,13 +30,17 @@ fi
 
 ./dbms2csv.py produto_loteexpedicao_referencias.ini _produto_loteexpedicao_referencias.csv $VERBOSE
 
-./dbms2csv.py produto_fabric_capa.ini _produto_fabric_capa.csv $VERBOSE
+./dbms2csv.py produto_fabric_pa_capa.ini _produto_fabric_pa_capa.csv $VERBOSE
+
+./dbms2csv.py produto_fabric_md_capa.ini _produto_fabric_md_capa.csv $VERBOSE
 
 ./dbms2csv.py produto_pa_capa.ini _produto_pa_capa.csv $VERBOSE
 
 ./dbms2csv.py produto_pa_tamanho.ini _produto_pa_tamanho.csv $VERBOSE
 
 ./dbms2csv.py produto_pa_tamanho_cor.ini _produto_pa_tamanho_cor.csv $VERBOSE
+
+./dbms2csv.py produto_md_capa.ini _produto_md_capa.csv $VERBOSE
 
 # delete
 
@@ -61,6 +65,8 @@ fi
 ./csv2oracle.py csv/_produto_pa_tamanho.csv yaml/basi_020.produtos_pa_nivel_1_tamanho.yaml -d $VERBOSE
 
 ./csv2oracle.py csv/_produto_pa_capa.csv  yaml/basi_030.produtos_pa_nivel_1.yaml -d $VERBOSE
+
+./csv2oracle.py csv/_produto_md_capa.csv  yaml/basi_030.produtos_md_nivel_1.yaml -d $VERBOSE
 
 ./csv2oracle.py ncm.fixo.csv -d $VERBOSE
 
@@ -100,11 +106,13 @@ fi
 
 ./csv2oracle.py ncm.fixo.csv -i $VERBOSE
 
+./csv2oracle.py csv/_produto_md_capa.csv  yaml/basi_030.produtos_md_nivel_1.yaml -i $VERBOSE
+
 ./csv2oracle.py csv/_produto_pa_capa.csv yaml/basi_030.produtos_pa_nivel_1.yaml -i $VERBOSE
 
 ./csv2oracle.py csv/_produto_pa_tamanho.csv yaml/basi_020.produtos_pa_nivel_1_tamanho.yaml -i $VERBOSE
 
-./csv2oracle.py csv/_produto_pa_tamanho_cor.csv yaml/basi_010.produtos_pa_nivel_1_tamanho_cor.yaml -d $VERBOSE
+./csv2oracle.py csv/_produto_pa_tamanho_cor.csv yaml/basi_010.produtos_pa_nivel_1_tamanho_cor.yaml -i $VERBOSE
 
 ./csv2oracle.py _produto_colecao.csv -i $VERBOSE
 
