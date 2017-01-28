481a5ca596cf7f61a6e02537694f5b7d4d750858
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2017-01-28 14:15:07 -0200

    lighter

--- tuple-load.sh
+++ tuple-load.sh
@@ -8,45 +8,45 @@ fi
 
 # generate
 
-./dbms2csv.py insumo_nao_tecido_codigos.ini _insumo_nao_tecido_codigos.csv $VERBOSE
+./dbms2csv.py insumo_nao_tecido_codigos $VERBOSE
 
-./dbms2csv.py insumo_nao_tecido_capa.ini _insumo_nao_tecido_capa.csv $VERBOSE
+./dbms2csv.py insumo_nao_tecido_capa $VERBOSE
 
-./dbms2csv.py insumo_nao_tecido_tamanho.ini _insumo_nao_tecido_tamanho.csv $VERBOSE
+./dbms2csv.py insumo_nao_tecido_tamanho $VERBOSE
 
-./dbms2csv.py insumo_nao_tecido_tamanho_cor.ini _insumo_nao_tecido_tamanho_cor.csv $VERBOSE
+./dbms2csv.py insumo_nao_tecido_tamanho_cor $VERBOSE
 
-./dbms2csv.py insumo_tecido_codigos.ini _insumo_tecido_codigos.csv $VERBOSE
+./dbms2csv.py insumo_tecido_codigos $VERBOSE
 
-./dbms2csv.py insumo_tecido_capa.ini _insumo_tecido_capa.csv $VERBOSE
+./dbms2csv.py insumo_tecido_capa $VERBOSE
 
-./dbms2csv.py insumo_tecido_tamanho.ini _insumo_tecido_tamanho.csv $VERBOSE
+./dbms2csv.py insumo_tecido_tamanho $VERBOSE
 
-./dbms2csv.py insumo_tecido_tamanho_cor.ini _insumo_tecido_tamanho_cor.csv $VERBOSE
+./dbms2csv.py insumo_tecido_tamanho_cor $VERBOSE
 
-./dbms2csv.py insumo_tecido_tamanho_cor_col.ini _insumo_tecido_tamanho_cor_col.csv $VERBOSE
+./dbms2csv.py insumo_tecido_tamanho_cor_col $VERBOSE
 
-./dbms2csv.py produto_colecao.ini _produto_colecao.csv $VERBOSE
+./dbms2csv.py produto_colecao $VERBOSE
 
-./dbms2csv.py produto_loteexpedicao_referencias.ini _produto_loteexpedicao_referencias.csv $VERBOSE
+./dbms2csv.py produto_loteexpedicao_referencias $VERBOSE
 
-./dbms2csv.py produto_fabric_pa_capa.ini _produto_fabric_pa_capa.csv $VERBOSE
+./dbms2csv.py produto_fabric_pa_capa $VERBOSE
 
-./dbms2csv.py produto_fabric_md_capa.ini _produto_fabric_md_capa.csv $VERBOSE
+./dbms2csv.py produto_fabric_md_capa $VERBOSE
 
-./dbms2csv.py produto_pa_capa.ini _produto_pa_capa.csv $VERBOSE
+./dbms2csv.py produto_pa_capa $VERBOSE
 
-./dbms2csv.py produto_pa_tamanho.ini _produto_pa_tamanho.csv $VERBOSE
+./dbms2csv.py produto_pa_tamanho $VERBOSE
 
-./dbms2csv.py produto_pa_tamanho_cor.ini _produto_pa_tamanho_cor.csv $VERBOSE
+./dbms2csv.py produto_pa_tamanho_cor $VERBOSE
 
-./dbms2csv.py produto_md_capa.ini _produto_md_capa.csv $VERBOSE
+./dbms2csv.py produto_md_capa $VERBOSE
 
-./dbms2csv.py produto_md_tamanho.ini _produto_md_tamanho.csv $VERBOSE
+./dbms2csv.py produto_md_tamanho $VERBOSE
 
-./dbms2csv.py produto_md_tamanho_cor.ini _produto_md_tamanho_cor.csv $VERBOSE
+./dbms2csv.py produto_md_tamanho_cor $VERBOSE
 
-./dbms2csv.py produto_pa_estrutura.ini _produto_pa_estrutura.csv $VERBOSE
+./dbms2csv.py produto_pa_estrutura $VERBOSE
 
 # delete
 
