8a53b5c32f174588008fc5671767e6a8abc4019a
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2017-01-28 15:26:23 -0200

    Better bash script

--- tuple-load.sh
+++ tuple-load.sh
@@ -8,138 +8,174 @@ fi
 
 # generate
 
-./dbms2csv.py insumo_nao_tecido_codigos $VERBOSE
+echo
+echo "====="
+echo "Generate CSVs"
+echo
+read -p "Confirm executing 'Generate'? (c/n) " -n1 -s exec
+echo
 
-./dbms2csv.py insumo_nao_tecido_capa $VERBOSE
+if [ $exec = 'c' ]; then
 
-./dbms2csv.py insumo_nao_tecido_tamanho $VERBOSE
+  # Order matters - Criation order
+  iniFiles=(
+    "insumo_nao_tecido_codigos"
+    "insumo_nao_tecido_capa"
+    "insumo_nao_tecido_tamanho"
+    "insumo_nao_tecido_tamanho_cor"
+    "insumo_tecido_codigos"
+    "insumo_tecido_capa"
+    "insumo_tecido_tamanho"
+    "insumo_tecido_tamanho_cor"
+    "insumo_tecido_tamanho_cor_col"
+    "produto_colecao"
+    "produto_loteexpedicao_referencias"
+    "produto_fabric_pa_capa"
+    "produto_fabric_md_capa"
+    "produto_pa_capa"
+    "produto_pa_tamanho"
+    "produto_pa_tamanho_cor"
+    "produto_md_capa"
+    "produto_md_tamanho"
+    "produto_md_tamanho_cor"
+    "produto_pa_estrutura"
+  )
 
-./dbms2csv.py insumo_nao_tecido_tamanho_cor $VERBOSE
+  echo
+  for iniFile in "${iniFiles[@]}"
+  do
+    echo
+    echo "====="
+    echo "./dbms2csv.py ${iniFile} $VERBOSE"
+    echo
+    read -p "Execute this command? (y/n) " -n1 -s exec
+    echo
+    echo
+    if [ $exec = 'y' ]; then
+      ./dbms2csv.py "${iniFile}" $VERBOSE
+    else
+      echo "Junp ${iniFile}"
+    fi
+  done
+  echo
 
-./dbms2csv.py insumo_tecido_codigos $VERBOSE
-
-./dbms2csv.py insumo_tecido_capa $VERBOSE
-
-./dbms2csv.py insumo_tecido_tamanho $VERBOSE
-
-./dbms2csv.py insumo_tecido_tamanho_cor $VERBOSE
-
-./dbms2csv.py insumo_tecido_tamanho_cor_col $VERBOSE
-
-./dbms2csv.py produto_colecao $VERBOSE
-
-./dbms2csv.py produto_loteexpedicao_referencias $VERBOSE
-
-./dbms2csv.py produto_fabric_pa_capa $VERBOSE
-
-./dbms2csv.py produto_fabric_md_capa $VERBOSE
-
-./dbms2csv.py produto_pa_capa $VERBOSE
-
-./dbms2csv.py produto_pa_tamanho $VERBOSE
-
-./dbms2csv.py produto_pa_tamanho_cor $VERBOSE
-
-./dbms2csv.py produto_md_capa $VERBOSE
-
-./dbms2csv.py produto_md_tamanho $VERBOSE
-
-./dbms2csv.py produto_md_tamanho_cor $VERBOSE
-
-./dbms2csv.py produto_pa_estrutura $VERBOSE
+fi
 
 # delete
 
-./csv2oracle.py _insumo_nao_tecido_tamanho_cor -d $VERBOSE
+echo
+echo "====="
+echo "Delete tuples no longer useful"
+echo
+read -p "Confirm executing 'Delete'? (c/n) " -n1 -s exec
+echo
 
-./csv2oracle.py _insumo_nao_tecido_tamanho -d $VERBOSE
+if [ $exec = 'c' ]; then
 
-./csv2oracle.py _insumo_nao_tecido_capa -d $VERBOSE
+  ./csv2oracle.py _insumo_nao_tecido_tamanho_cor -d $VERBOSE
 
-./csv2oracle.py _insumo_tecido_tamanho_cor_col -d $VERBOSE
+  ./csv2oracle.py _insumo_nao_tecido_tamanho -d $VERBOSE
 
-./csv2oracle.py _insumo_tecido_tamanho_cor -d $VERBOSE
+  ./csv2oracle.py _insumo_nao_tecido_capa -d $VERBOSE
 
-./csv2oracle.py _insumo_tecido_tamanho -d $VERBOSE
+  ./csv2oracle.py _insumo_tecido_tamanho_cor_col -d $VERBOSE
 
-./csv2oracle.py _insumo_tecido_capa -d $VERBOSE
+  ./csv2oracle.py _insumo_tecido_tamanho_cor -d $VERBOSE
 
-./csv2oracle.py _produto_pa_tamanho_cor -d $VERBOSE
+  ./csv2oracle.py _insumo_tecido_tamanho -d $VERBOSE
 
-./csv2oracle.py _produto_pa_tamanho -d $VERBOSE
+  ./csv2oracle.py _insumo_tecido_capa -d $VERBOSE
 
-./csv2oracle.py _produto_pa_capa  -d $VERBOSE
+  ./csv2oracle.py _produto_pa_tamanho_cor -d $VERBOSE
 
-./csv2oracle.py _produto_md_tamanho_cor -d $VERBOSE
+  ./csv2oracle.py _produto_pa_tamanho -d $VERBOSE
 
-./csv2oracle.py _produto_md_tamanho -d $VERBOSE
+  ./csv2oracle.py _produto_pa_capa  -d $VERBOSE
 
-./csv2oracle.py _produto_md_capa  -d $VERBOSE
+  ./csv2oracle.py _produto_md_tamanho_cor -d $VERBOSE
 
-./csv2oracle.py _produto_colecao -d $VERBOSE
+  ./csv2oracle.py _produto_md_tamanho -d $VERBOSE
 
-./csv2oracle.py ncm.fixo -d $VERBOSE
+  ./csv2oracle.py _produto_md_capa  -d $VERBOSE
 
-./csv2oracle.py produto-artigo_de_produto -d $VERBOSE
+  ./csv2oracle.py _produto_colecao -d $VERBOSE
 
-./csv2oracle.py tecido-artigo_de_produto -d $VERBOSE
+  ./csv2oracle.py ncm.fixo -d $VERBOSE
 
-./csv2oracle.py produto-linha_de_produto -d $VERBOSE
+  ./csv2oracle.py produto-artigo_de_produto -d $VERBOSE
 
-./csv2oracle.py tecido-linha_de_produto -d $VERBOSE
+  ./csv2oracle.py tecido-artigo_de_produto -d $VERBOSE
 
-./csv2oracle.py conta_de_estoque.v161202 -d $VERBOSE
+  ./csv2oracle.py produto-linha_de_produto -d $VERBOSE
 
-./csv2oracle.py cor.fixo -d $VERBOSE
+  ./csv2oracle.py tecido-linha_de_produto -d $VERBOSE
 
-./csv2oracle.py tamanho.fixo -d $VERBOSE
+  ./csv2oracle.py conta_de_estoque.v161202 -d $VERBOSE
 
-./csv2oracle.py unidades_de_medida.fixo -d $VERBOSE
+  ./csv2oracle.py cor.fixo -d $VERBOSE
+
+  ./csv2oracle.py tamanho.fixo -d $VERBOSE
+
+  ./csv2oracle.py unidades_de_medida.fixo -d $VERBOSE
+
+fi
 
 # insert/update
 
-./csv2oracle.py unidades_de_medida.fixo -i $VERBOSE
+echo
+echo "====="
+echo "Insert/Update tuples"
+echo
+read -p "Confirm executing 'Insert/Update'? (c/n) " -n1 -s exec
+echo
 
-./csv2oracle.py tamanho.fixo -i $VERBOSE
+if [ $exec = 'c' ]; then
 
-./csv2oracle.py cor.fixo -i $VERBOSE
+  ./csv2oracle.py unidades_de_medida.fixo -i $VERBOSE
 
-./csv2oracle.py conta_de_estoque.v161202 -i $VERBOSE
+  ./csv2oracle.py tamanho.fixo -i $VERBOSE
 
-./csv2oracle.py tecido-linha_de_produto -i $VERBOSE
+  ./csv2oracle.py cor.fixo -i $VERBOSE
 
-./csv2oracle.py produto-linha_de_produto -i $VERBOSE
+  ./csv2oracle.py conta_de_estoque.v161202 -i $VERBOSE
 
-./csv2oracle.py tecido-artigo_de_produto -i $VERBOSE
+  ./csv2oracle.py tecido-linha_de_produto -i $VERBOSE
 
-./csv2oracle.py produto-artigo_de_produto -i $VERBOSE
+  ./csv2oracle.py produto-linha_de_produto -i $VERBOSE
 
-./csv2oracle.py ncm.fixo -i $VERBOSE
+  ./csv2oracle.py tecido-artigo_de_produto -i $VERBOSE
 
-./csv2oracle.py _produto_colecao -i $VERBOSE
+  ./csv2oracle.py produto-artigo_de_produto -i $VERBOSE
 
-./csv2oracle.py _produto_md_capa -i $VERBOSE
+  ./csv2oracle.py ncm.fixo -i $VERBOSE
 
-./csv2oracle.py _produto_md_tamanho -i $VERBOSE
+  ./csv2oracle.py _produto_colecao -i $VERBOSE
 
-./csv2oracle.py _produto_md_tamanho_cor -i $VERBOSE
+  ./csv2oracle.py _produto_md_capa -i $VERBOSE
 
-./csv2oracle.py _produto_pa_capa -i $VERBOSE
+  ./csv2oracle.py _produto_md_tamanho -i $VERBOSE
 
-./csv2oracle.py _produto_pa_tamanho -i $VERBOSE
+  ./csv2oracle.py _produto_md_tamanho_cor -i $VERBOSE
 
-./csv2oracle.py _produto_pa_tamanho_cor -i $VERBOSE
+  ./csv2oracle.py _produto_pa_capa -i $VERBOSE
 
-./csv2oracle.py _insumo_tecido_capa -i $VERBOSE
+  ./csv2oracle.py _produto_pa_tamanho -i $VERBOSE
 
-./csv2oracle.py _insumo_tecido_tamanho -i $VERBOSE
+  ./csv2oracle.py _produto_pa_tamanho_cor -i $VERBOSE
 
-./csv2oracle.py _insumo_tecido_tamanho_cor -i $VERBOSE
+  ./csv2oracle.py _insumo_tecido_capa -i $VERBOSE
 
-./csv2oracle.py csv/_insumo_tecido_tamanho_cor_col -i $VERBOSE
+  ./csv2oracle.py _insumo_tecido_tamanho -i $VERBOSE
 
-./csv2oracle.py _insumo_nao_tecido_capa -i $VERBOSE
+  ./csv2oracle.py _insumo_tecido_tamanho_cor -i $VERBOSE
 
-./csv2oracle.py _insumo_nao_tecido_tamanho -i $VERBOSE
+  ./csv2oracle.py csv/_insumo_tecido_tamanho_cor_col -i $VERBOSE
 
-./csv2oracle.py _insumo_nao_tecido_tamanho_cor -i $VERBOSE
+  ./csv2oracle.py _insumo_nao_tecido_capa -i $VERBOSE
+
+  ./csv2oracle.py _insumo_nao_tecido_tamanho -i $VERBOSE
+
+  ./csv2oracle.py _insumo_nao_tecido_tamanho_cor -i $VERBOSE
+
+fi
+echo
