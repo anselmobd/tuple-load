c6ec2c39fa8619b2affe8371d40471be925d64f7
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2017-01-28 13:29:48 -0200

    Adding fileWithRequiredExtension function

--- tuple-load.sh
+++ tuple-load.sh
@@ -50,96 +50,96 @@ fi
 
 # delete
 
-./csv2oracle.py _insumo_nao_tecido_tamanho_cor.csv -d $VERBOSE
+./csv2oracle.py _insumo_nao_tecido_tamanho_cor -d $VERBOSE
 
-./csv2oracle.py _insumo_nao_tecido_tamanho.csv -d $VERBOSE
+./csv2oracle.py _insumo_nao_tecido_tamanho -d $VERBOSE
 
-./csv2oracle.py _insumo_nao_tecido_capa.csv -d $VERBOSE
+./csv2oracle.py _insumo_nao_tecido_capa -d $VERBOSE
 
-./csv2oracle.py _insumo_tecido_tamanho_cor_col.csv -d $VERBOSE
+./csv2oracle.py _insumo_tecido_tamanho_cor_col -d $VERBOSE
 
-./csv2oracle.py _insumo_tecido_tamanho_cor.csv -d $VERBOSE
+./csv2oracle.py _insumo_tecido_tamanho_cor -d $VERBOSE
 
-./csv2oracle.py _insumo_tecido_tamanho.csv -d $VERBOSE
+./csv2oracle.py _insumo_tecido_tamanho -d $VERBOSE
 
-./csv2oracle.py _insumo_tecido_capa.csv -d $VERBOSE
+./csv2oracle.py _insumo_tecido_capa -d $VERBOSE
 
-./csv2oracle.py _produto_pa_tamanho_cor.csv -d $VERBOSE
+./csv2oracle.py _produto_pa_tamanho_cor -d $VERBOSE
 
-./csv2oracle.py _produto_pa_tamanho.csv -d $VERBOSE
+./csv2oracle.py _produto_pa_tamanho -d $VERBOSE
 
-./csv2oracle.py _produto_pa_capa.csv  -d $VERBOSE
+./csv2oracle.py _produto_pa_capa  -d $VERBOSE
 
-./csv2oracle.py _produto_md_tamanho_cor.csv -d $VERBOSE
+./csv2oracle.py _produto_md_tamanho_cor -d $VERBOSE
 
-./csv2oracle.py _produto_md_tamanho.csv -d $VERBOSE
+./csv2oracle.py _produto_md_tamanho -d $VERBOSE
 
-./csv2oracle.py _produto_md_capa.csv  -d $VERBOSE
+./csv2oracle.py _produto_md_capa  -d $VERBOSE
 
-./csv2oracle.py _produto_colecao.csv -d $VERBOSE
+./csv2oracle.py _produto_colecao -d $VERBOSE
 
-./csv2oracle.py ncm.fixo.csv -d $VERBOSE
+./csv2oracle.py ncm.fixo -d $VERBOSE
 
-./csv2oracle.py produto-artigo_de_produto.csv -d $VERBOSE
+./csv2oracle.py produto-artigo_de_produto -d $VERBOSE
 
-./csv2oracle.py tecido-artigo_de_produto.csv -d $VERBOSE
+./csv2oracle.py tecido-artigo_de_produto -d $VERBOSE
 
-./csv2oracle.py produto-linha_de_produto.csv -d $VERBOSE
+./csv2oracle.py produto-linha_de_produto -d $VERBOSE
 
-./csv2oracle.py tecido-linha_de_produto.csv -d $VERBOSE
+./csv2oracle.py tecido-linha_de_produto -d $VERBOSE
 
-./csv2oracle.py conta_de_estoque.v161202.csv -d $VERBOSE
+./csv2oracle.py conta_de_estoque.v161202 -d $VERBOSE
 
-./csv2oracle.py cor.fixo.csv -d $VERBOSE
+./csv2oracle.py cor.fixo -d $VERBOSE
 
-./csv2oracle.py tamanho.fixo.csv -d $VERBOSE
+./csv2oracle.py tamanho.fixo -d $VERBOSE
 
-./csv2oracle.py unidades_de_medida.fixo.csv -d $VERBOSE
+./csv2oracle.py unidades_de_medida.fixo -d $VERBOSE
 
 # insert/update
 
-./csv2oracle.py unidades_de_medida.fixo.csv -i $VERBOSE
+./csv2oracle.py unidades_de_medida.fixo -i $VERBOSE
 
-./csv2oracle.py tamanho.fixo.csv -i $VERBOSE
+./csv2oracle.py tamanho.fixo -i $VERBOSE
 
-./csv2oracle.py cor.fixo.csv -i $VERBOSE
+./csv2oracle.py cor.fixo -i $VERBOSE
 
-./csv2oracle.py conta_de_estoque.v161202.csv -i $VERBOSE
+./csv2oracle.py conta_de_estoque.v161202 -i $VERBOSE
 
-./csv2oracle.py tecido-linha_de_produto.csv -i $VERBOSE
+./csv2oracle.py tecido-linha_de_produto -i $VERBOSE
 
-./csv2oracle.py produto-linha_de_produto.csv -i $VERBOSE
+./csv2oracle.py produto-linha_de_produto -i $VERBOSE
 
-./csv2oracle.py tecido-artigo_de_produto.csv -i $VERBOSE
+./csv2oracle.py tecido-artigo_de_produto -i $VERBOSE
 
-./csv2oracle.py produto-artigo_de_produto.csv -i $VERBOSE
+./csv2oracle.py produto-artigo_de_produto -i $VERBOSE
 
-./csv2oracle.py ncm.fixo.csv -i $VERBOSE
+./csv2oracle.py ncm.fixo -i $VERBOSE
 
-./csv2oracle.py _produto_colecao.csv -i $VERBOSE
+./csv2oracle.py _produto_colecao -i $VERBOSE
 
-./csv2oracle.py _produto_md_capa.csv -i $VERBOSE
+./csv2oracle.py _produto_md_capa -i $VERBOSE
 
-./csv2oracle.py _produto_md_tamanho.csv -i $VERBOSE
+./csv2oracle.py _produto_md_tamanho -i $VERBOSE
 
-./csv2oracle.py _produto_md_tamanho_cor.csv -i $VERBOSE
+./csv2oracle.py _produto_md_tamanho_cor -i $VERBOSE
 
-./csv2oracle.py _produto_pa_capa.csv -i $VERBOSE
+./csv2oracle.py _produto_pa_capa -i $VERBOSE
 
-./csv2oracle.py _produto_pa_tamanho.csv -i $VERBOSE
+./csv2oracle.py _produto_pa_tamanho -i $VERBOSE
 
-./csv2oracle.py _produto_pa_tamanho_cor.csv -i $VERBOSE
+./csv2oracle.py _produto_pa_tamanho_cor -i $VERBOSE
 
-./csv2oracle.py _insumo_tecido_capa.csv -i $VERBOSE
+./csv2oracle.py _insumo_tecido_capa -i $VERBOSE
 
-./csv2oracle.py _insumo_tecido_tamanho.csv -i $VERBOSE
+./csv2oracle.py _insumo_tecido_tamanho -i $VERBOSE
 
-./csv2oracle.py _insumo_tecido_tamanho_cor.csv -i $VERBOSE
+./csv2oracle.py _insumo_tecido_tamanho_cor -i $VERBOSE
 
-./csv2oracle.py csv/_insumo_tecido_tamanho_cor_col.csv -i $VERBOSE
+./csv2oracle.py csv/_insumo_tecido_tamanho_cor_col -i $VERBOSE
 
-./csv2oracle.py _insumo_nao_tecido_capa.csv -i $VERBOSE
+./csv2oracle.py _insumo_nao_tecido_capa -i $VERBOSE
 
-./csv2oracle.py _insumo_nao_tecido_tamanho.csv -i $VERBOSE
+./csv2oracle.py _insumo_nao_tecido_tamanho -i $VERBOSE
 
-./csv2oracle.py _insumo_nao_tecido_tamanho_cor.csv -i $VERBOSE
+./csv2oracle.py _insumo_nao_tecido_tamanho_cor -i $VERBOSE
