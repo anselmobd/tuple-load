b82b091ef6f93089b483e349ad34b623d6689278
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2017-02-04 08:07:52 -0200

    Beginning of several improvements in the code of class Cnpj

--- oxy/usual.py
+++ oxy/usual.py
@@ -2,6 +2,7 @@
 # -*- coding: utf8 -*-
 
 import os
+import re
 from pprint import pprint
 
 
@@ -41,60 +42,59 @@ class VerboseOutput:
 
 
 class Cnpj:
+    """
+    Initially inspired by Murtog from http://wiki.python.org.br/Cnpj
+    Adapted and tested under Python3.4 in 2017-02-04 by Anselmo B. D.
+    """
     def __init__(self):
         """
-        Class to interact with Cnpj brazilian numbers
+        Class to interact with CNPJ brazilian numbers
         """
         pass
 
     def validate(self, cnpj):
         """
-        Method to validate brazilian cnpjs
-        Tests:
-
-        >>> print Cnpj().validate('61882613000194')
-        True
-        >>> print Cnpj().validate('61882613000195')
-        False
-        >>> print Cnpj().validate('53.612.734/0001-98')
-        True
-        >>> print Cnpj().validate('69.435.154/0001-02')
-        True
-        >>> print Cnpj().validate('69.435.154/0001-01')
-        False
+        Method to validate brazilian CNPJs.
+        Argument cnpj must contain 14 or 15 digits.
+        Other characters wil be ignored.
+        Sample tests:
+            print(Cnpj().validate('61882613000194'))  # True
+            print(Cnpj().validate('061882613000194'))  # True
+            print(Cnpj().validate('61882613000195'))  # False
+            print(Cnpj().validate('53.612.734/0001-98'))  # True
+            print(Cnpj().validate('69.435.154/0001-02'))  # True
+            print(Cnpj().validate('69.435.154/0001-01'))  # False
         """
-
-        # returnig
         return bool(cnpj[-2:] == self.digits(cnpj))
 
     def digits(self, cnpj):
         """
-        Method to validate brazilian cnpjs
+        Method to calculate the last two digits from brazilian CNPJs.
+        Argument cnpj must contain from 12 to 15 digits.
+        "cnpj" argument with less than 14 digits will be considered without
+        check digits. With 14 or more will be considered with the digits and
+        these will be ignored.
         Tests:
-
-        >>> print Cnpj().validate('61882613000194')
-        True
-        >>> print Cnpj().validate('61882613000195')
-        False
-        >>> print Cnpj().validate('53.612.734/0001-98')
-        True
-        >>> print Cnpj().validate('69.435.154/0001-02')
-        True
-        >>> print Cnpj().validate('69.435.154/0001-01')
-        False
+            print(Cnpj().digits('61882613000194'))  # 94
+            print(Cnpj().digits('618826130001'))  # 94
+            print(Cnpj().digits('061882613000194'))  # 49
+            print(Cnpj().digits('61882613000195'))  # 94
+            print(Cnpj().digits('53.612.734/0001-98'))  # 98
+            print(Cnpj().digits('69.435.154/0001-02'))  # 02
+            print(Cnpj().digits('69.435.154/0001-01'))  # 02
         """
         # defining some variables
         lista_validacao_um = [5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2]
         lista_validacao_dois = [6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2]
 
         # cleaning the cnpj
-        cnpj = cnpj.replace("-", "")
-        cnpj = cnpj.replace(".", "")
-        cnpj = cnpj.replace("/", "")
+        cnpj = re.sub("[^0-9]", "", cnpj)
 
         # verifying the lenght of the cnpj
-        if len(cnpj) < 12:
-            return False
+        if len(cnpj) < 12 or len(cnpj) > 15:
+            raise ValueError(
+                'Argument cnpj "{}" must contain from 12 to 15 digits '
+                'and contain {}'.format(cnpj, len(cnpj)))
 
         # calculating the first digit
         soma = 0
