fceee205d862aa21cf6b01c658602536a6a38ce2
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2017-03-31 20:29:22 -0300

    arg parse

--- prod.sh
+++ prod.sh
@@ -1,6 +1,39 @@
 #!/bin/bash
 # tuzle.sh subject_destination{.sh} [-verbose]
 
+getopt --test > /dev/null
+if [[ $? -ne 4 ]]; then
+    echo "I’m sorry, `getopt --test` failed in this environment."
+    exit 1
+fi
+SHORT=gv
+LONG=global,verbose
+PARSED=$(getopt --options $SHORT --longoptions $LONG --name "$0" -- "$@")
+if [[ $? -ne 0 ]]; then
+    exit 2
+fi
+eval set -- "$PARSED"
+while true; do
+    case "$1" in
+        -g|--global)
+            exec=g
+            shift
+            ;;
+        -v|--verbose)
+            VERBOSE="-${VERBOSE#-}v"
+            shift
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            echo "Programming error"
+            exit 3
+            ;;
+    esac
+done
+
 # To dbms2csv.py
 # Order matters - Criation order
 iniFiles=(
@@ -11,23 +44,47 @@ iniFiles=(
 dataGroupFiles=(
 )
 
-if [ $1 ] ; then
-  VERBOSE=$1
-else
-  VERBOSE=""
+if [ -z ${exec+x} ] ; then
+  exec='#'
 fi
 
-echo "./tuzle.sh _user_prod.sh $VERBOSE"
-./tuzle.sh _user_prod.sh $VERBOSE
+echo
+if [ $exec != 'g' ] ; then
+  read -p "Confirm executing 'prod' tuzle? (c/n/g) " -n1 -s exec
+  echo
+fi
 
-echo "./tuzle.sh _basico_prod.sh $VERBOSE"
-./tuzle.sh _basico_prod.sh $VERBOSE
+if [ $exec = 'g' ] ; then
+  if [ -z ${INIFULL+x} ] ; then
+    INIFULL=$(date)
+  fi
+  EXECARG='-g'
+fi
 
-echo "./tuzle.sh _contab_prod.sh $VERBOSE"
-./tuzle.sh _contab_prod.sh $VERBOSE
+if [ $exec = 'c' -o $exec = 'g' ] ; then
 
-echo "./tuzle.sh _produto_prod.sh $VERBOSE"
-./tuzle.sh _produto_prod.sh $VERBOSE
+  echo "./tuzle.sh _user_prod.sh $VERBOSE $EXECARG"
+  ./tuzle.sh _user_prod.sh $VERBOSE $EXECARG
 
-echo "./tuzle.sh _comercial_prod.sh $VERBOSE"
-./tuzle.sh _comercial_prod.sh $VERBOSE
+  echo "./tuzle.sh _basico_prod.sh $VERBOSE $EXECARG"
+  ./tuzle.sh _basico_prod.sh $VERBOSE $EXECARG
+
+  echo "./tuzle.sh _contab_prod.sh $VERBOSE $EXECARG"
+  ./tuzle.sh _contab_prod.sh $VERBOSE $EXECARG
+
+  echo "./tuzle.sh _produto_prod.sh $VERBOSE $EXECARG"
+  ./tuzle.sh _produto_prod.sh $VERBOSE $EXECARG
+
+  echo "./tuzle.sh _comercial_prod.sh $VERBOSE $EXECARG"
+  ./tuzle.sh _comercial_prod.sh $VERBOSE $EXECARG
+
+fi
+
+if [ $exec = 'g' ] ; then
+  echo "$0 started at:"
+  echo $INIFULL
+  echo
+  echo "$0 ended at:"
+  date
+  echo
+fi
