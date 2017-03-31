0fe97b3ed49015e7fa536a324bdbf727849bb7ab
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2017-03-31 10:27:02 -0300

    evolving

--- tuzle.sh
+++ tuzle.sh
@@ -44,135 +44,148 @@ else
   VERBOSE=""
 fi
 
-# generate
+if [ ${#iniFile[@]} -ne 0 ]; then
 
-echo
-echo "====="
-echo "Generate CSVs"
-echo
-read -p "Confirm executing 'Generate'? (c/n/a/g) " -n1 -s exec
-echo
-
-if [ $exec = 'g' ]; then
-  INI=$(date)
-fi
-
-if [ $exec = 'c' -o $exec = 'a' -o $exec = 'g' ]; then
+  # generate
 
   echo
-  for iniFile in "${iniFiles[@]}"
-  do
+  echo "====="
+  echo "Generate CSVs"
+  echo
+  read -p "Confirm executing 'Generate'? (c/n/a/g) " -n1 -s exec
+  echo
+
+  if [ $exec = 'g' ]; then
+    INI=$(date)
+  fi
+
+  if [ $exec = 'c' -o $exec = 'a' -o $exec = 'g' ]; then
+
     echo
-    echo "====="
-    echo "./dbms2csv.py ${iniFile} $VERBOSE"
-    echo
-    if [ $exec != 'a' -a $exec != 'g' ]; then
-      read -p "Execute this command? (y/n) " -n1 -s exec
+    for iniFile in "${iniFiles[@]}"
+    do
       echo
-    fi
-    echo
-    if [ $exec = 'y' -o $exec = 'a' -o $exec = 'g' ]; then
-      ./dbms2csv.py ${iniFile} $VERBOSE
-      if [ $? -eq 0 ]; then
-        echo
-        echo "Generated data group ${iniFile} - OK !!!"
-      else
-        echo
-        echo "ERROR generating data group ${iniFile} !!!"
-        exit
-      fi
-    else
-      echo "Jump ${iniFile}"
-    fi
-  done
-  echo
-
-fi
-
-# delete
-
-echo
-echo "====="
-echo "Delete tuples no longer useful"
-echo
-if [ $exec != 'g' ]; then
-  read -p "Confirm executing 'Delete'? (c/n/a) " -n1 -s exec
-  echo
-fi
-
-if [ $exec = 'c' -o $exec = 'a' -o $exec = 'g' ]; then
-
-  echo
-  for (( idx=${#dataGroupFiles[@]}-1 ; idx>=0 ; idx-- )) ; do
-    echo
-    echo "====="
-    echo "./csv2oracle.py ${dataGroupFiles[idx]} -d $VERBOSE $DBVAR"
-    echo
-    if [ $exec != 'a' -a $exec != 'g' ]; then
-      read -p "Execute this command? (y/n) " -n1 -s exec
+      echo "====="
+      echo "./dbms2csv.py ${iniFile} $VERBOSE"
       echo
-    fi
-    echo
-    if [ $exec = 'y' -o $exec = 'a' -o $exec = 'g' ]; then
-      ./csv2oracle.py ${dataGroupFiles[idx]} -d $VERBOSE $DBVAR
-      if [ $? -eq 0 ]; then
+      if [ $exec != 'a' -a $exec != 'g' ]; then
+        read -p "Execute this command? (y/n) " -n1 -s exec
         echo
-        echo "Deleted data group ${dataGroupFiles[idx]} - OK !!!"
-      else
-        echo
-        echo "ERROR deleting data group ${dataGroupFiles[idx]} !!!"
-        exit
       fi
-    else
-      echo "Jump ${dataGroupFiles[idx]}"
-    fi
-  done
-  echo
-
-fi
-
-# insert/update
-
-echo
-echo "====="
-echo "Insert/Update tuples"
-echo
-if [ $exec != 'g' ]; then
-  read -p "Confirm executing 'Insert/Update'? (c/n/a) " -n1 -s exec
-  echo
-fi
-
-if [ $exec = 'c' -o $exec = 'a' -o $exec = 'g' ]; then
-
-  echo
-  for dataGroupFile in "${dataGroupFiles[@]}"
-  do
-    echo
-    echo "====="
-    echo "./csv2oracle.py ${dataGroupFile} -i $VERBOSE $DBVAR"
-    echo
-    if [ $exec != 'a' -a $exec != 'g' ]; then
-      read -p "Execute this command? (y/n) " -n1 -s exec
       echo
-    fi
-    echo
-    if [ $exec = 'y' -o $exec = 'a' -o $exec = 'g' ]; then
-      ./csv2oracle.py ${dataGroupFile} -i $VERBOSE $DBVAR
-      if [ $? -eq 0 ]; then
-        echo
-        echo "Inserted/Updated data group ${dataGroupFile} - OK !!!"
+      if [ $exec = 'y' -o $exec = 'a' -o $exec = 'g' ]; then
+        ./dbms2csv.py ${iniFile} $VERBOSE
+        if [ $? -eq 0 ]; then
+          echo
+          echo "Generated data group ${iniFile} - OK !!!"
+        else
+          echo
+          echo "ERROR generating data group ${iniFile} !!!"
+          exit
+        fi
       else
-        echo
-        echo "ERROR Inserting/Updating data group ${dataGroupFile} !!!"
-        exit
+        echo "Jump ${iniFile}"
       fi
-    else
-      echo "Jump ${dataGroupFile}"
-    fi
-  done
-  echo
+    done
+    echo
+
+  fi
 
 fi
+
+if [ ${#dataGroupFiles[@]} -ne 0 ]; then
+
+  if [ -z ${exec+x} ] ; then
+    exec='#'
+  fi
+
+  # delete
+
+  echo
+  echo "====="
+  echo "Delete tuples no longer useful"
+  echo
+  if [ $exec != 'g' ]; then
+    read -p "Confirm executing 'Delete'? (c/n/a) " -n1 -s exec
+    echo
+  fi
+
+  if [ $exec = 'c' -o $exec = 'a' -o $exec = 'g' ]; then
+
+    echo
+    for (( idx=${#dataGroupFiles[@]}-1 ; idx>=0 ; idx-- )) ; do
+      echo
+      echo "====="
+      echo "./csv2oracle.py ${dataGroupFiles[idx]} -d $VERBOSE $DBVAR"
+      echo
+      if [ $exec != 'a' -a $exec != 'g' ]; then
+        read -p "Execute this command? (y/n) " -n1 -s exec
+        echo
+      fi
+      echo
+      if [ $exec = 'y' -o $exec = 'a' -o $exec = 'g' ]; then
+        ./csv2oracle.py ${dataGroupFiles[idx]} -d $VERBOSE $DBVAR
+        if [ $? -eq 0 ]; then
+          echo
+          echo "Deleted data group ${dataGroupFiles[idx]} - OK !!!"
+        else
+          echo
+          echo "ERROR deleting data group ${dataGroupFiles[idx]} !!!"
+          exit
+        fi
+      else
+        echo "Jump ${dataGroupFiles[idx]}"
+      fi
+    done
+    echo
+
+  fi
+
+  # insert/update
+
+  echo
+  echo "====="
+  echo "Insert/Update tuples"
+  echo
+  if [ $exec != 'g' ]; then
+    read -p "Confirm executing 'Insert/Update'? (c/n/a) " -n1 -s exec
+    echo
+  fi
+
+  if [ $exec = 'c' -o $exec = 'a' -o $exec = 'g' ]; then
+
+    echo
+    for dataGroupFile in "${dataGroupFiles[@]}"
+    do
+      echo
+      echo "====="
+      echo "./csv2oracle.py ${dataGroupFile} -i $VERBOSE $DBVAR"
+      echo
+      if [ $exec != 'a' -a $exec != 'g' ]; then
+        read -p "Execute this command? (y/n) " -n1 -s exec
+        echo
+      fi
+      echo
+      if [ $exec = 'y' -o $exec = 'a' -o $exec = 'g' ]; then
+        ./csv2oracle.py ${dataGroupFile} -i $VERBOSE $DBVAR
+        if [ $? -eq 0 ]; then
+          echo
+          echo "Inserted/Updated data group ${dataGroupFile} - OK !!!"
+        else
+          echo
+          echo "ERROR Inserting/Updating data group ${dataGroupFile} !!!"
+          exit
+        fi
+      else
+        echo "Jump ${dataGroupFile}"
+      fi
+    done
+    echo
+
+  fi
+
+fi
+
 echo
 
 if [ $exec = 'g' ]; then
