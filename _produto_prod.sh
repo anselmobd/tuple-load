1a184bbfb5988ac4063a1675a896eb8e7c68a0bc
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2017-04-12 13:44:08 -0300

    will be just to PA and MD

--- _produto_prod.sh
+++ _produto_prod.sh
@@ -5,24 +5,11 @@ DBVAR='--dbvar prod'
 # To dbms2csv.py
 # Order matters - Criation order
 iniFiles=(
+
 )
 
 # To csv2oracle.py
 # Order matters - Criation order
 dataGroupFiles=(
-  "--inicio"
-  "conta_de_estoque"
-  "faixa_etaria.fixo"
-  "unidades_de_medida.fixo"
-  "tamanho.fixo"
-  "serie_tamanho.fixo"
-  "serie_tamanho_tamanho.fixo"
-  "cor.fixo"
-  "ncm.fixo"
-  "tecido-linha_de_produto"
-  "tecido-artigo_de_produto"
-  "produto-linha_de_produto"
-  "produto-artigo_de_produto"
-  "produto_colecao"
-  "--fim"
+
 )
