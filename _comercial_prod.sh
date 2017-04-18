18b97a03181bb3034a7910fdacfaf192e6a6871b
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2017-04-17 23:45:03 -0300

    cliente

--- _comercial_prod.sh
+++ _comercial_prod.sh
@@ -5,12 +5,16 @@ DBVAR='--dbvar prod'
 # To dbms2csv.py
 # Order matters - Criation order
 iniFiles=(
+  "--inicio"
+  "cliente"
+  "--fim"
 )
 
 # To csv2oracle.py
 # Order matters - Criation order
 dataGroupFiles=(
   "--inicio"
+  "_cliente"
   "periodo_de_comissao"
   "periodo_de_producao_por_area"
   "--fim"
