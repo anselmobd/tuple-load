cfe99f41849bc1d3289de298220df8f5ea6f0f16
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2017-05-12 15:51:01 -0300

    ean

--- _ean_prod.sh
+++ _ean_prod.sh
@@ -1,6 +1,10 @@
+#!/bin/bash
+
 ./csv2oracle.py ean_havan_tam -vvvv --dbvar prod
 ./csv2oracle.py ean_havan_cor_tam -vvvv --dbvar prod
 
 ./csv2oracle.py ean_nao_havan -vvvv --dbvar prod
 ./csv2oracle.py ean_nao_havan_tam -vvvv --dbvar prod
 ./csv2oracle.py ean_nao_havan_cor_tam -vvvv --dbvar prod
+
+./csv2oracle.py ean_0706B_0717B_05156_cor_tam -vvvv --dbvar prod
