33dc9d66b7ac5f0a68b92d64ae6715cee30a9395
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2017-05-19 15:57:40 -0300

    infadprod

---
+++ sql/038 infAdProd carga CEA.sql
@@ -0,0 +1,14 @@
+INSERT INTO ESTQ_400 ( CLIENTE9, CLIENTE4, CLIENTE2, TERCEIRO, NIVEL_ESTRUTURA, GRUPO_ESTRUTURA, SUBGRUPO_ESTRUTURA, ITEM_ESTRUTURA, REF_CLIENTE, PRECO_CLIENTE, IMPRIME_DESC_CLI_DANFE
+)         select  45242914 , 42 , 75  , 0  , '1'  , '5156J', 'P',  '0000BR', '8291080'  , 0  , 2  from dual
+union all select  45242914 , 42 , 75  , 0  , '1'  , '5156J', 'M',  '0000BR', '8291079'  , 0  , 2  from dual
+union all select  45242914 , 42 , 75  , 0  , '1'  , '5156J', 'G',  '0000BR', '8291077'  , 0  , 2  from dual
+union all select  45242914 , 42 , 75  , 0  , '1'  , '5156J', 'GG', '0000BR', '8291078'  , 0  , 2  from dual
+union all select  45242914 , 42 , 75  , 0  , '1'  , '5156J', 'P',  '0000ME', '8291080'  , 0  , 2  from dual
+union all select  45242914 , 42 , 75  , 0  , '1'  , '5156J', 'M',  '0000ME', '8291079'  , 0  , 2  from dual
+union all select  45242914 , 42 , 75  , 0  , '1'  , '5156J', 'G',  '0000ME', '8291077'  , 0  , 2  from dual
+union all select  45242914 , 42 , 75  , 0  , '1'  , '5156J', 'GG', '0000ME', '8291078'  , 0  , 2  from dual
+union all select  45242914 , 42 , 75  , 0  , '1'  , '5156J', 'P',  '0000BC', '8291080'  , 0  , 2  from dual
+union all select  45242914 , 42 , 75  , 0  , '1'  , '5156J', 'M',  '0000BC', '8291079'  , 0  , 2  from dual
+union all select  45242914 , 42 , 75  , 0  , '1'  , '5156J', 'G',  '0000BC', '8291077'  , 0  , 2  from dual
+union all select  45242914 , 42 , 75  , 0  , '1'  , '5156J', 'GG', '0000BC', '8291078'  , 0  , 2  from dual
+;
