dbe9537f542bd2736d1595cd6421102cc62ef46f
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2017-05-19 18:32:49 -0300

    bug

--- sql/037 infAdProd segunda carga Renner.sql
+++ sql/037 infAdProd segunda carga Renner.sql
@@ -1,22 +1,22 @@
 INSERT INTO ESTQ_400 ( CLIENTE9, CLIENTE4, CLIENTE2, TERCEIRO, NIVEL_ESTRUTURA, GRUPO_ESTRUTURA, SUBGRUPO_ESTRUTURA, ITEM_ESTRUTURA, REF_CLIENTE, PRECO_CLIENTE, IMPRIME_DESC_CLI_DANFE
-)         select  92754738 , 203 , 50  , 0  , '1'  , '0257R', 'P',  '0000BR', '539742577'  , 0  , 2  from dual
-union all select  92754738 , 203 , 50  , 0  , '1'  , '0257R', 'M',  '0000BR', '539742585'  , 0  , 2  from dual
-union all select  92754738 , 203 , 50  , 0  , '1'  , '0257R', 'G',  '0000BR', '539742593'  , 0  , 2  from dual
-union all select  92754738 , 203 , 50  , 0  , '1'  , '0257R', 'GG', '0000BR', '539742606'  , 0  , 2  from dual
+)         select  92754738 , 203 , 50  , 0  , '1'  , '0257R', 'G',  '0000BR', '539742577'  , 0  , 2  from dual
+union all select  92754738 , 203 , 50  , 0  , '1'  , '0257R', 'GG', '0000BR', '539742585'  , 0  , 2  from dual
+union all select  92754738 , 203 , 50  , 0  , '1'  , '0257R', 'M',  '0000BR', '539742593'  , 0  , 2  from dual
+union all select  92754738 , 203 , 50  , 0  , '1'  , '0257R', 'P',  '0000BR', '539742606'  , 0  , 2  from dual
 union all select  92754738 , 203 , 50  , 0  , '1'  , '0257R', 'P',  '0000AZ', '539280755'  , 0  , 2  from dual
 union all select  92754738 , 203 , 50  , 0  , '1'  , '0257R', 'M',  '0000AZ', '539280763'  , 0  , 2  from dual
 union all select  92754738 , 203 , 50  , 0  , '1'  , '0257R', 'G',  '0000AZ', '539280771'  , 0  , 2  from dual
 union all select  92754738 , 203 , 50  , 0  , '1'  , '0257R', 'GG', '0000AZ', '539280780'  , 0  , 2  from dual
-union all select  92754738 , 203 , 50  , 0  , '1'  , '0257R', 'P',  '0000PR', '539742500'  , 0  , 2  from dual
-union all select  92754738 , 203 , 50  , 0  , '1'  , '0257R', 'M',  '0000PR', '539742518'  , 0  , 2  from dual
-union all select  92754738 , 203 , 50  , 0  , '1'  , '0257R', 'G',  '0000PR', '539742526'  , 0  , 2  from dual
-union all select  92754738 , 203 , 50  , 0  , '1'  , '0257R', 'GG', '0000PR', '539742534'  , 0  , 2  from dual
+union all select  92754738 , 203 , 50  , 0  , '1'  , '0257R', 'G',  '0000PR', '539742500'  , 0  , 2  from dual
+union all select  92754738 , 203 , 50  , 0  , '1'  , '0257R', 'GG', '0000PR', '539742518'  , 0  , 2  from dual
+union all select  92754738 , 203 , 50  , 0  , '1'  , '0257R', 'M',  '0000PR', '539742526'  , 0  , 2  from dual
+union all select  92754738 , 203 , 50  , 0  , '1'  , '0257R', 'P',  '0000PR', '539742534'  , 0  , 2  from dual
 ;
 INSERT INTO ESTQ_400 ( CLIENTE9, CLIENTE4, CLIENTE2, TERCEIRO, NIVEL_ESTRUTURA, GRUPO_ESTRUTURA, SUBGRUPO_ESTRUTURA, ITEM_ESTRUTURA, REF_CLIENTE, PRECO_CLIENTE, IMPRIME_DESC_CLI_DANFE
-)         select  92754738 , 203 , 50  , 0  , '1'  , '0257R', 'P',  '0000PE', '539773761'  , 0  , 2  from dual
-union all select  92754738 , 203 , 50  , 0  , '1'  , '0257R', 'M',  '0000PE', '539773779'  , 0  , 2  from dual
-union all select  92754738 , 203 , 50  , 0  , '1'  , '0257R', 'G',  '0000PE', '539773787'  , 0  , 2  from dual
-union all select  92754738 , 203 , 50  , 0  , '1'  , '0257R', 'GG', '0000PE', '539773795'  , 0  , 2  from dual
+)         select  92754738 , 203 , 50  , 0  , '1'  , '0257R', 'G',  '0000PE', '539773761'  , 0  , 2  from dual
+union all select  92754738 , 203 , 50  , 0  , '1'  , '0257R', 'GG', '0000PE', '539773779'  , 0  , 2  from dual
+union all select  92754738 , 203 , 50  , 0  , '1'  , '0257R', 'M',  '0000PE', '539773787'  , 0  , 2  from dual
+union all select  92754738 , 203 , 50  , 0  , '1'  , '0257R', 'P',  '0000PE', '539773795'  , 0  , 2  from dual
 ;
 INSERT INTO ESTQ_400 ( CLIENTE9, CLIENTE4, CLIENTE2, TERCEIRO, NIVEL_ESTRUTURA, GRUPO_ESTRUTURA, SUBGRUPO_ESTRUTURA, ITEM_ESTRUTURA, REF_CLIENTE, PRECO_CLIENTE, IMPRIME_DESC_CLI_DANFE
 )         select  92754738 , 203 , 50  , 0  , '1'  , '0717R', 'P',  '0000BR', '542344254'  , 0  , 2  from dual
