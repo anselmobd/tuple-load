3e0fa3c80e7485ec267b1d5e0a3d558edca32816
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2017-05-26 16:59:27 -0300

    indadprod

---
+++ sql/041 infAdProd terceira carga Renner.sql
@@ -0,0 +1,14 @@
+INSERT INTO ESTQ_400 ( CLIENTE9, CLIENTE4, CLIENTE2, TERCEIRO, NIVEL_ESTRUTURA, GRUPO_ESTRUTURA, SUBGRUPO_ESTRUTURA, ITEM_ESTRUTURA, REF_CLIENTE, PRECO_CLIENTE, IMPRIME_DESC_CLI_DANFE
+)         select  92754738 , 203 , 50  , 0  , '1'  , '5156R', 'P',  '0000BR', '535214970'  , 0  , 2  from dual
+union all select  92754738 , 203 , 50  , 0  , '1'  , '5156R', 'P',  '0000PR', '535214988'  , 0  , 2  from dual
+union all select  92754738 , 203 , 50  , 0  , '1'  , '5156R', 'P',  '0000ME', '535214996'  , 0  , 2  from dual
+union all select  92754738 , 203 , 50  , 0  , '1'  , '5156R', 'M',  '0000BR', '535215008'  , 0  , 2  from dual
+union all select  92754738 , 203 , 50  , 0  , '1'  , '5156R', 'M',  '0000PR', '535215016'  , 0  , 2  from dual
+union all select  92754738 , 203 , 50  , 0  , '1'  , '5156R', 'M',  '0000ME', '535215024'  , 0  , 2  from dual
+union all select  92754738 , 203 , 50  , 0  , '1'  , '5156R', 'G',  '0000BR', '535215032'  , 0  , 2  from dual
+union all select  92754738 , 203 , 50  , 0  , '1'  , '5156R', 'G',  '0000PR', '535215041'  , 0  , 2  from dual
+union all select  92754738 , 203 , 50  , 0  , '1'  , '5156R', 'G',  '0000ME', '535215059'  , 0  , 2  from dual
+union all select  92754738 , 203 , 50  , 0  , '1'  , '5156R', 'GG', '0000BR', '535215067'  , 0  , 2  from dual
+union all select  92754738 , 203 , 50  , 0  , '1'  , '5156R', 'GG', '0000PR', '535215075'  , 0  , 2  from dual
+union all select  92754738 , 203 , 50  , 0  , '1'  , '5156R', 'GG', '0000ME', '535215083'  , 0  , 2  from dual
+;
