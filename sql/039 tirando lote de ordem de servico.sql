c72ff357ea03db144bc18a2524108d9781622fac
Anselmo Blanco Dominguez <anselmo.blanco.dominguez+github@gmail.com>
2017-06-23 06:31:34 -0300

    os

--- sql/039 tirando lote de ordem de servico.sql
+++ sql/039 tirando lote de ordem de servico.sql
@@ -1,4 +1,8 @@
 -- !!! Verificar na view se já tem data de faturamento da OS. Se tem, não deve excluir.
+
+-- !!! ATENÇÃO: Não é só marcar e desmarcar os lotes no estágio, tem que cuidar
+-- das tabelas obrf_081 e obrf_082
+
 SELECT DISTINCT
   l.ORDEM_PRODUCAO
 , l.NUMERO_ORDEM
@@ -7,18 +11,18 @@ SELECT DISTINCT
 , l.ORDEM_CONFECCAO
 , l.*
 FROM pcpc_040 l
---UPDATE pcpc_040 l
---SET
---  l.NUMERO_ORDEM = 0
---, l.SEQ_ORDEM_SERV = 0
-WHERE l.ORDEM_PRODUCAO = 297
-  AND l.NUMERO_ORDEM = 170
-  AND l.PERIODO_PRODUCAO = 1719
+WHERE 1=1
+  --AND l.ORDEM_PRODUCAO = 297
+  AND l.NUMERO_ORDEM = 302
+  AND l.CODIGO_ESTAGIO = 39
+  AND l.PERIODO_PRODUCAO = 1721
   AND l.ORDEM_CONFECCAO IN (
-    9811
-  , 9812
-  , 9813
+    3679
+  , 3708
   )
+  AND l.NUMERO_ORDEM <> 0
+  AND l.SEQ_ORDEM_SERV <> 0
+;
 
 --
 
@@ -27,14 +31,89 @@ SET
   l.NUMERO_ORDEM = 0
 , l.SEQ_ORDEM_SERV = 0
 WHERE 1=1
-  -- AND l.ORDEM_PRODUCAO = 297
-  -- AND l.NUMERO_ORDEM = 170
-  AND l.PERIODO_PRODUCAO = 1722
+  --AND l.ORDEM_PRODUCAO = 297
+  AND l.NUMERO_ORDEM = 302
+  AND l.CODIGO_ESTAGIO = 39
+  AND l.PERIODO_PRODUCAO = 1721
   AND l.ORDEM_CONFECCAO IN (
-    4107
-  , 4099
-  , 4015
-  , 4242
+    3679
+  , 3708
   )
   AND l.NUMERO_ORDEM <> 0
   AND l.SEQ_ORDEM_SERV <> 0
+;
+
+-- estudando tabelas da OS
+
+-- lotes mancados com a OS
+
+SELECT DISTINCT
+  l.ORDEM_PRODUCAO
+, l.NUMERO_ORDEM
+, l.SEQ_ORDEM_SERV
+, l.PERIODO_PRODUCAO
+, l.ORDEM_CONFECCAO
+, l.*
+FROM pcpc_040 l -- lotes nos estágios
+WHERE 1=1
+  --AND l.ORDEM_PRODUCAO = 297
+  AND l.NUMERO_ORDEM = 375
+  AND l.CODIGO_ESTAGIO = 39
+  AND l.PERIODO_PRODUCAO = 1721
+--  AND l.ORDEM_CONFECCAO IN (
+--    3770
+--
+--  )
+ORDER BY
+  l.ORDEM_CONFECCAO
+;
+
+-- capa da OS
+
+SELECT
+  DISTINCT
+  s.CODIGO_SERVICO
+, s.CGCTERC_FORNE9
+--, s.*
+FROM OBRF_080 s
+--WHERE s.NUMERO_ORDEM = 375
+ORDER BY
+  s.CODIGO_SERVICO
+, s.CGCTERC_FORNE9
+;
+
+-- itens da OS - só nivel 1 - tudo que volta
+
+SELECT
+  DISTINCT
+  s.PRODORD_NIVEL99
+, s.PRODORD_GRUPO
+, s.PRODORD_SUBGRUPO
+, s.PRODORD_ITEM
+--, s.*
+FROM OBRF_081 s
+--WHERE s.NUMERO_ORDEM = 375
+ORDER BY
+  s.PRODORD_NIVEL99
+, s.PRODORD_GRUPO
+, s.PRODORD_SUBGRUPO
+, s.PRODORD_ITEM
+;
+
+-- itens da OS - todos os níveis - tudo que  sai
+
+SELECT
+  DISTINCT
+  s.PRODSAI_NIVEL99
+, s.PRODSAI_GRUPO
+, s.PRODSAI_SUBGRUPO
+, s.PRODSAI_ITEM
+--, s.*
+FROM OBRF_082 s
+--WHERE s.NUMERO_ORDEM = 375
+ORDER BY
+  s.PRODSAI_NIVEL99
+, s.PRODSAI_GRUPO
+, s.PRODSAI_SUBGRUPO
+, s.PRODSAI_ITEM
+;
